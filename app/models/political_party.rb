1dad5ef49fb89446b0f6dd6df191ca75012ce1d7
Virginia Rodriguez <virginia88@gmail.com>
2014-11-15 11:05:44 -0200

    Change main text. Use real data for displaying count of legislators

--- app/models/political_party.rb
+++ app/models/political_party.rb
@@ -1,9 +1,18 @@
 class PoliticalParty < ActiveRecord::Base
+  has_many :legislators
 
   def self.load_seeds
     PoliticalParty.create({name: "partido nacional"})
     PoliticalParty.create({name: "partido colorado"})
     PoliticalParty.create({name: "partido frente amplio"})
     PoliticalParty.create({name: "partido independiente"})
+  end
+
+  def senadores_count
+    legislators.in_senadores.count
+  end
+
+  def diputados_count
+    legislators.in_diputados.count
   end
 end

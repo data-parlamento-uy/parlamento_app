1bc99e8a387024acbf06f15e37df762d6afa0a5b
Virginia Rodriguez <virginia88@gmail.com>
2014-11-15 11:42:42 -0200

    Use real data for displaying count of legislators and state rep

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,9 +21,31 @@ module ApplicationHelper
     end
   end
 
+  def left_flag_image(political_party)
+    case political_party.name
+      when 'partido nacional'      then 'bandera_pn.svg'
+      when 'partido colorado'      then 'bandera_pc.svg'
+      when 'partido independiente' then 'bandera_pi.svg'
+      when 'partido frente amplio' then 'bandera_fa.svg'
+    end
+  end
+
   def pretty_text(text)
     text.capitalize!
     exc = %w(y con el de para del se la sobre a las que)
     text.split(" ").map {|word| exc.include?(word) ? word : word.capitalize }.join(" ")
+  end
+
+  def state_abbr(state_name)
+    case state_name
+      when "cerro largo" then 'crl'
+      when "flores" then 'fls'
+      when "florida" then "fla"
+      when "montevideo" then "mvd"
+      when "rio negro" then "rne"
+      when "san jose" then "saj"
+      when "treinta y tres" then "tyt"
+      else state_name[0..2]
+    end
   end
 end
--- app/models/political_party.rb
+++ app/models/political_party.rb
@@ -15,4 +15,17 @@ class PoliticalParty < ActiveRecord::Base
   def diputados_count
     legislators.in_diputados.count
   end
+
+  def pretty_name
+    case name
+      when 'partido nacional'      then 'p.nacional'
+      when 'partido colorado'      then 'p.colorado'
+      when 'partido independiente' then 'p.independiente'
+      when 'partido frente amplio' then 'frente amplio'
+    end
+  end
+
+  def related_states
+    legislators.in_diputados.map(&:state).uniq.sort
+  end
 end
